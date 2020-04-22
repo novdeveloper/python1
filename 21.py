4b472f536d007605856e119608036ae051b61e9c
yuyov777@mail.ru <yuyov777@mail.ru>
2020-04-22 17:55:53 +0500

    отредактировал отступы 21 задание, но не работает

--- 21.py
+++ 21.py
@@ -25,18 +25,18 @@ def __repr__(self):
     return "Car('%s', %s, '%s')" % (self.make, self.year, self.body)
 
 #основная программа, присваиваем списки к переменным    
-honda = Car("honda", 1999, "universal")
-toyota = Car("toyota", 1995, "sedan")
-lada = Car("lada", 2020, "sedan")
-seat = Car("seat", 2002, "universal")
+    honda = Car("honda", 1999, "universal")
+    toyota = Car("toyota", 1995, "sedan")
+    lada = Car("lada", 2020, "sedan")
+    seat = Car("seat", 2002, "universal")
 
 #описываем ключи словаря
-cars = {
-        honda.key: honda,
-        toyota.key: toyota,
-        lada.key: lada,
-        seat.key: seat,
-        }
+    cars = {
+            honda.key: honda,
+            toyota.key: toyota,
+            lada.key: lada,
+            seat.key: seat,
+            }
 #вывод в консоль
-pprint(cars)
-pprint(cars[seat.key])
+    pprint(cars)
+    pprint(cars[seat.key])
