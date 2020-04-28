a21dbffa6429ffda53bfad08540d374afc03df4d
yuyov777@mail.ru <yuyov777@mail.ru>
2020-04-29 01:15:50 +0500

    отредактировал отступы 21 задание, заработало

--- 21.py
+++ 21.py
@@ -5,7 +5,11 @@ from pprint import pprint
 #создаётся класс, под названием 'Car'
 
 class Car:
-    pass
+    #создаём функцию, конструктор класса, без него программа не будет работат
+
+    def __init__(self, make, year, body):
+        self.make, self.year, self.body = make, year, body
+        self.key = (make, body)
 
 #создаём подкласс(наследование)
 
@@ -27,32 +31,28 @@ class Lada(Car):
 class Seat(Car):
     pass
     
-#создаём функцию, конструктор класса, без него программа не будет работат
 
-def __init__(self, make, year, body):
-    self.make, self.year, self.body = make, year, body
-    self.key = (make, body)
 
 #создаём функцию, отвечающую за строковое представление вывода
 
-def __repr__(self):
-    return "Car('%s', %s, '%s')" % (self.make, self.year, self.body)
+    def __repr__(self):
+        return "Car('%s', %s, '%s')" % (self.make, self.year, self.body)
 
 #основная программа, присваиваем списки к переменным    
 
-    honda = Car("honda", 1999, "universal")
-    toyota = Car("toyota", 1995, "sedan")
-    lada = Car("lada", 2020, "sedan")
-    seat = Car("seat", 2002, "universal")
+honda = Car("honda", 1999, "universal")
+toyota = Car("toyota", 1995, "sedan")
+lada = Car("lada", 2020, "sedan")
+seat = Car("seat", 2002, "universal")
 
 #описываем ключи словаря
 
-    cars = {
-            honda.key: honda,
-            toyota.key: toyota,
-            lada.key: lada,
-            seat.key: seat,
-            }
+cars = {
+        honda.key: honda,
+        toyota.key: toyota,
+        lada.key: lada,
+        seat.key: seat,
+       }
 
 #вывод в консоль
 
